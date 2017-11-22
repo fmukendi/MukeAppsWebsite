2c1c046d9d90a2718d31ad16a8968dd81321ef50
Franck Mukendi <franck.mukendi@gmail.com>
2017-11-22 00:47:53 -0500

    working navbar mutation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,27 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app.routing';
 
 
 import { AppComponent } from './app.component';
-import { NavBarComponent } from './shared/nav/navbar.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
 import { LandingComponent } from './landing/landing.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent, 
-    NavBarComponent, 
-    LandingComponent
+    NavbarComponent, 
+    LandingComponent, 
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     NgbModule.forRoot(),
     FormsModule,
     RouterModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
